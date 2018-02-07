870704901e93ef909b585fea165480a74801813c
jqiao2 <jqiao2@gmail.com>
2018-02-07 14:27:41 -0800

    Updated SSC

---
+++ subredditSubscriberCounter.py
@@ -0,0 +1,25 @@
+import praw
+
+# Make your own profile file with all these variables for oauth 2
+from profile import CLIENT_ID
+from profile import CLIENT_SECRET
+from profile import PASSWORD
+from profile import USERAGENT
+from profile import USERNAME
+
+r = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD,
+                user_agent = USERAGENT, username = USERNAME)
+
+
+def get_subs():
+    subreddits = open('subreddits.txt')
+    for line in subreddits:
+        line = line.rstrip('\n')
+        try:
+            sc = r.subreddit(line).subscribers
+            print(line + ": " + str(sc))
+        except:
+            print(line)
+
+
+get_subs()
